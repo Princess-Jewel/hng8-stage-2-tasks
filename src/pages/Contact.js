f81cad60343f02af87eb1bd2181c4f82bf0971ed
Princess-Jewel <princessjewel80@gmail.com>
2021-02-24 16:37:13 +0100

    i made some changes to the contact component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,12 +15,22 @@ const Contact = () =>
         <div className="col-md-4 info">
 
           <div className="info-box">
-            <p> <img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306305/email_otd9pj.svg"} alt="pic" className="image_2" /><span>PrincessJewel80@gmail.com</span></p>
+              <p>
+                <a className="mail" href="mailto:princessjewel80@gmail.com"> <img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306305/email_otd9pj.svg"} alt="pic" className="image_2 mail" /><span>PrincessJewel80@gmail.com</span></a>
+              </p>
           </div>
 
           <div className="info-box">
-            <p><img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306321/phone_cv21sh.svg"} alt="pic" className="image_2" /><span>+2349061312216</span></p>
-            <p><img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306321/phone_cv21sh.svg"} alt="pic" className="image_2" /><span>+2347083368633</span></p>
+              <p>
+              <a className="mail" href="tel:+2349061312216">
+              <img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306321/phone_cv21sh.svg"} alt="pic" className="image_2" /><span>+2349061312216</span>
+              </a>
+              </p>
+              <p>
+              <a className="mail" href="tel:+2347083368633">
+              <img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306321/phone_cv21sh.svg"} alt="pic" className="image_2" /><span>+2347083368633</span>
+              </a>
+              </p>
           </div>
 
           <div className="info-box">
@@ -31,7 +41,8 @@ const Contact = () =>
             <ul>
               <li><a href="https://linkedin.com/in/princess-jewel-jel-edema" target="_blank" rel="noopener noreferrer"> <img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306321/linkedin_oe1h3e.svg"} alt="pic" className="image_3" /></a></li>
               <li><a href="https://twitter.com/thatebonychic/" target="_blank" rel="noopener noreferrer"> <img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306322/twitter_kl30sw.svg"} alt="pic" className="image_3" /></a></li>
-              <li><a href="https://github.com/Princess-Jewel/" target="_blank" rel="noopener noreferrer"> <img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306307/github_twjnkw.svg"} alt="pic" className="image_3" /></a></li>
+              <li><a href="https://github.com/Princess-Jewel/" target="_blank" rel="noopener noreferrer"> <img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306307/github_twjnkw.svg"} alt="pic" className="image_3" /></a></li>  
+             
             </ul>
           </div>
 
