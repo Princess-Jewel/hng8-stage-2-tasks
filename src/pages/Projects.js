bf0cca4c4b75767c1762bf37d9b4b63c9f6affb4
Princess-Jewel <princessjewel80@gmail.com>
2021-08-22 10:12:12 +0100

    updated the projects section

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,43 +1,10 @@
 import React from 'react';
 import Layout from '../layout';
-import TypedReactDemo from './TypedReactDemo';
-import About from '../pages/About';
-import Contact from '../pages/Contact';
 
 
-
-const Home = () =>
-
-  <Layout>
-    <section className="container home">
-      <div className="row align-items-center home1">
-        {/* min-vh-100 min-vw-100 */}
-        <div className="col-md-6">
-          <img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306966/vector_kflkvj.jpg"} alt="pic" className="img-fluid image_second" />
-        </div>
-
-        <div className="col-md-6 text-center home_1">
-          <TypedReactDemo
-            strings={[
-              'Princess Jewel Jel-Edema',
-              'A Front-End Developer',
-              'Open To Work Anywhere!',
-
-
-            ]}
-          />
-          <p>I am an independent self-starter team player who is organized, detail oriented and able to work in a dynamic environment.
-          I have hands-on experience working with React.js. I also have experience converting Figma and Adobe XD design mockups to HTML and CSS.
-          </p>
-         
-        </div>
-        <div className="col-md-6">
-          <img src={"https://res.cloudinary.com/dpqxraalv/image/upload/v1609306966/vector_kflkvj.jpg"} alt="pic" className="img-fluid image" />
-        </div>
-      </div>
-    </section>
+const Projects = () => 
     <Layout>
-    <section className="container projects">
+          <section className="container projects">
       <div><h1>Projects</h1></div>
       <div className="row">
 
@@ -127,10 +94,7 @@ const Home = () =>
       </div>
     </section>
     </Layout>
+;
 
-    <About/>
-    <Contact/>
-  </Layout>
-  ;
 
-export default Home;
+export default Projects;
